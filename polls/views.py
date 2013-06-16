46af18c94b0158d929d720b85d9aca3acc9e5673
Shibly <shibly.phy@gmail.com>
2013-06-16 15:13:43 +0600

    Implemented Generic Views

--- polls/views.py
+++ polls/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import Http404, get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from polls.models import Choice, Poll
+from django.views import generic
 
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """
+        Returns the last five published polls
+        """
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-    return render(request, 'polls/detail.html', {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 
 def vote(request, poll_id):
